127bb6a6e94363a377f8a40f32ad4eb1531176fd
tianzhaojie <1192556828@qq.com>
2022-09-25 10:23:25 +0800

    feat:初始化

---
+++ src/store/modules/user.js
@@ -0,0 +1,23 @@
+
+const getDefaultState = () => {
+
+}
+
+const state = getDefaultState()
+
+const mutations = {
+
+}
+
+const actions = {
+  // user login
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
